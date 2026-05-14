ef0a8bb5e2052fa896dfeff0eb4cfa62e47b0ec4
drdiegopadua <drdiegopadua@gmail.com>
2026-05-14 16:36:29 -0300

    Adicionada animação nos números

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Supercopa AFC carregada!");
+const counters = document.querySelectorAll('.stat-box h2');
+
+counters.forEach(counter => {
+
+  const updateCounter = () => {
+
+    const target = +counter.innerText;
+    const current = +counter.getAttribute('data-count') || 0;
+
+    const increment = target / 80;
+
+    if(current < target){
+
+      const newValue = Math.ceil(current + increment);
+
+      counter.setAttribute('data-count', newValue);
+      counter.innerText = newValue;
+
+      setTimeout(updateCounter, 25);
+
+    }else{
+
+      counter.innerText = target;
+
+    }
+
+  };
+
+  updateCounter();
+
+});
