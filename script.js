b216bbd8b4dab045ff9f88c30e9b7e26fe71f90a
drdiegopadua <drdiegopadua@gmail.com>
2026-05-15 16:00:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log("Supercopa AFC carregada com sucesso!");
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("Supercopa AFC carregada com sucesso!");
+
+  const counters = document.querySelectorAll(".counter-num");
+
+  const animateCounter = (counter) => {
+    const target = +counter.getAttribute("data-target");
+    let current = 0;
+
+    const speed = target / 120; // controla velocidade
+
+    const update = () => {
+      current += speed;
+
+      if (current < target) {
+        counter.innerText = Math.floor(current).toLocaleString("pt-BR");
+        requestAnimationFrame(update);
+      } else {
+        counter.innerText = target.toLocaleString("pt-BR");
+      }
+    };
+
+    update();
+  };
+
+  const section = document.querySelector(".counter-section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+
+        counters.forEach(counter => {
+          animateCounter(counter);
+        });
+
+        observer.unobserve(section);
+      }
+    });
+  }, {
+    threshold: 0.4
+  });
+
+  if (section) {
+    observer.observe(section);
+  }
+
+});
